67e5375120964264670988da12103359e7dfb892
eric <meissner.eric.7@gmail.com>
2015-04-15 15:12:11 -0600

    Cleanup. Moved models.

--- platty/platty/views.py
+++ platty/platty/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext, loader
+from .models import Event
 
 def index(request):
         return HttpResponse('Hey there, you are at the home index!')
@@ -10,7 +11,14 @@ def home(request):
     return render_to_response('index.html', context_instance=RequestContext(request))
 
 def create(request):
-    return render_to_response('create.html', context_instance=RequestContext(request))
+ #   Event.objects.create(name= "Banana", location="The Congo", description="Why WOULDN'T you want a Congo banana
+ #           party?",)
+    event_list = Event.objects.order_by('-id')[:5]
+    template = loader.get_template('create.html')
+    context = RequestContext(request, {
+        'event_list': event_list,
+    })
+    return HttpResponse(template.render(context))
 
 def parties(request):
     return render_to_response('parties.html', context_instance=RequestContext(request))
